a205a4150ddae97fced45f69c5c3ad98eb5f5c4c
Lucino772 <lucapalmi772@gmail.com>
2020-12-26 19:22:22 +0000

    Connection wrapper fix

--- src/classes/connection.py
+++ src/classes/connection.py
@@ -7,23 +7,8 @@ except:
 
 class ConnectionWrapper:
 
-    def __init__(self, alias=None, client=None, user=None, passwd=None, lang=None, trace=None, ashost=None, sysnr=None, mshost=None, msserv=None, sysid=None, group=None, snc_qop=None, snc_myname=None, snc_partnername=None):
-        self.conn_info = {
-            'client': client,
-            'user': user,
-            'passwd': passwd,
-            'lang': lang,
-            'trace': trace,
-            'ashost': ashost,
-            'sysnr': sysnr,
-            'mshost': mshost,
-            'msserv': msserv,
-            'sysid': sysid,
-            'group': group,
-            'snc_qop': snc_qop,
-            'snc_myname': snc_myname,
-            'snc_partnername': snc_partnername
-        }
+    def __init__(self, alias=None, **conn_info):
+        self.conn_info = conn_info
         self.alias = alias
         self.conn = Connection(**self.conn_info)
         self.id = uuid.uuid4().hex 
